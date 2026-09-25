62c91776bbccf78253a0161a047bffe25d02c205
Jeremi_aron <chancanjeremiaron@gmail.com>
2026-09-24 21:10:52 -0500

    fix(app): 🐛 read API URL from VITE_API_URL env

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = import.meta.env.API_URL || 'http://localhost:3000';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 export const api = (path: string) => axios.create({
   baseURL: API_URL + path,
